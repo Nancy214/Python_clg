9d6740fb7255e55bfd0de5026e54ec443dff0d8f
Nancy Patel <64463889+Nancy214@users.noreply.github.com>
2020-05-22 17:02:22 +0530

    Add files via upload

---
+++ Assignment 1/Q7.py
@@ -0,0 +1,14 @@
+my_dict = {'apples':5, 'bananas':7, 'oranges':3, 'mangoes':10, 'peaches':2, 'watermelon':1}
+print('Copy of my_dict: ',my_dict.copy())
+print('Items:',my_dict.items())
+print('Keys:',my_dict.keys())
+print('Values:',my_dict.values())
+print('Pop:',my_dict.pop('watermelon'))
+print(my_dict)
+print('Update:',my_dict.update({'pineapple':1}))
+print(my_dict)
+print('Get:',my_dict.get('bananas'))
+print(my_dict.clear())
+
+
+
